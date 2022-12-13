cee354da40e0e56a1e00d504709fca4dc6b6195d
kenji Hashimoto <rath.miguel@gmail.com>
2022-12-13 20:10:24 +0900

    FTPに上がっているデータの引き上げ

--- src/templates/index.php
+++ src/templates/index.php
@@ -5,8 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="format-detection" content="telephone=no,address=no,email=no">
     <meta id="viewport" name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes">
+    <link rel="stylesheet" type="text/css" href="<?php echo esc_url(get_template_directory_uri());?>/css/vendor.css">
     <link rel="stylesheet" type="text/css" href="<?php echo esc_url(get_template_directory_uri());?>/css/style.css">
-    <title>森のスープ屋の夜 1組限定の宿</title>
+    <title>森のスープ屋の夜 お手当のための宿</title>
     <?php wp_head() ?>
   </head>
   <body <?php body_class() ?> id="home">
