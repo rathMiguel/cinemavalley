7bda43eb830eaa7b9b5127f98a8ff1f88589a387
kenji Hashimoto <rath.miguel@gmail.com>
2023-01-12 10:06:37 +0900

    モーダルの高さ調整

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   base: "/",
   publicDir: resolve(__dirname, 'public'),
   server: {
-    open: '/'
+    open: false
   },
   build: {
     outDir: resolve(__dirname, 'dist'),
